eccfbc9208c98bc83549601012def95cde9fbdd4
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2026-05-16 10:58:39 +0530

    Update embeddings.py

--- app/retrieval/embeddings.py
+++ app/retrieval/embeddings.py
@@ -1,7 +1,23 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+
+
+def get_model():
+
+    global model
+
+    if model is None:
+
+        model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    return model
 
 
 def embed_text(text):
+
+    model = get_model()
+
     return model.encode(text)
